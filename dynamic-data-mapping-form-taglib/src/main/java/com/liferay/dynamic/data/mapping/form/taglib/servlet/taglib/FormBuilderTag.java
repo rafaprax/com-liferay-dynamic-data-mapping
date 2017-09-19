6f34963a4f0fd47432cabaa5b89a22b8f30272d3
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-09-19 09:48:13 -0300

    LPS-74385 Initialize the formBuilder and RuleBuilder on demand

--- dynamic-data-mapping-form-taglib/src/main/java/com/liferay/dynamic/data/mapping/form/taglib/servlet/taglib/FormBuilderTag.java
+++ dynamic-data-mapping-form-taglib/src/main/java/com/liferay/dynamic/data/mapping/form/taglib/servlet/taglib/FormBuilderTag.java
@@ -14,11 +14,83 @@
 
 package com.liferay.dynamic.data.mapping.form.taglib.servlet.taglib;
 
+import com.liferay.dynamic.data.mapping.form.builder.settings.DDMFormBuilderSettingsRequest;
+import com.liferay.dynamic.data.mapping.form.builder.settings.DDMFormBuilderSettingsResponse;
 import com.liferay.dynamic.data.mapping.form.taglib.servlet.taglib.base.BaseFormBuilderTag;
+import com.liferay.dynamic.data.mapping.form.taglib.servlet.taglib.util.FormTaglibContextUtil;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Rafael Praxedes
  */
 public class FormBuilderTag extends BaseFormBuilderTag {
+
+	public String getDDMFormBuilderContext(ThemeDisplay themeDisplay) {
+		return FormTaglibContextUtil.getFormBuilderContext(
+			GetterUtil.getLong(getDdmStructureId()), themeDisplay);
+	}
+
+	protected DDMForm getDDMForm() {
+		return FormTaglibContextUtil.getDDMForm(
+			GetterUtil.getLong(getDdmStructureId()));
+	}
+
+	protected DDMFormBuilderSettingsResponse getDDMFormBuilderSettings(
+		HttpServletRequest request) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return FormTaglibContextUtil.getDDMFormBuilderSettings(
+			DDMFormBuilderSettingsRequest.with(
+				getDDMForm(), themeDisplay.getLocale()));
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		super.setAttributes(request);
+
+		DDMFormBuilderSettingsResponse ddmFormBuilderSettings =
+			getDDMFormBuilderSettings(request);
+
+		setNamespacedAttribute(
+			request, "dataProviderInstancesURL",
+			ddmFormBuilderSettings.getSetting("dataProviderInstancesURL"));
+		setNamespacedAttribute(
+			request, "dataProviderInstanceParameterSettingsURL",
+			ddmFormBuilderSettings.getSetting(
+				"dataProviderInstanceParameterSettingsURL"));
+		setNamespacedAttribute(
+			request, "evaluatorURL",
+			ddmFormBuilderSettings.getSetting("formContextProviderURL"));
+		setNamespacedAttribute(
+			request, "fieldSettingsDDMFormContextURL",
+			ddmFormBuilderSettings.getSetting(
+				"fieldSettingsDDMFormContextURL"));
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		setNamespacedAttribute(
+			request, "formBuilderContext",
+			getDDMFormBuilderContext(themeDisplay));
+
+		setNamespacedAttribute(
+			request, "functionsMetadata",
+			ddmFormBuilderSettings.getSetting("functionsMetadata"));
+		setNamespacedAttribute(
+			request, "functionsURL",
+			ddmFormBuilderSettings.getSetting("dataProviderInstancesURL"));
+		setNamespacedAttribute(
+			request, "rolesURL", ddmFormBuilderSettings.getSetting("rolesURL"));
+		setNamespacedAttribute(
+			request, "serializedDDMFormRules",
+			ddmFormBuilderSettings.getSetting("serializedDDMFormRules"));
+	}
 
 }
